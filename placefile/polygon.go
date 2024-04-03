5238e40ba2bc39f069c1fbe58da7c08a222962ae
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2024-04-02 19:16:24 -0500

    Adding polygon

--- placefile/polygon.go
+++ placefile/polygon.go
@@ -1,1 +1,30 @@
 package placefile
+
+import (
+	"fmt"
+)
+
+type Polygon struct {
+	R         int
+	G         int
+	B         int
+	A         int
+	Threshold int
+	latlon    string
+}
+
+func (p *Polygon) String() string {
+	var res string
+	res = fmt.Sprintf("Polygon: %d %d %d %d\n", p.R, p.G, p.B, p.A)
+	res += p.latlon
+	res += "End:\n"
+	return res
+}
+
+func (p *Polygon) AddRawLatLon(ll string) {
+	p.latlon = ll
+}
+
+func (p *Polygon) MarshalYAML() (interface{}, error) {
+	return p.String(), nil
+}
