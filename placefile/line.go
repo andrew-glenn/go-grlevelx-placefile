69e6753c180e9fe89c7556c337e20e9d1703d7cd
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2024-04-02 20:55:26 -0500

    Adding ColorLineWithText

--- placefile/line.go
+++ placefile/line.go
@@ -1,1 +1,34 @@
 package placefile
+
+import "fmt"
+
+type ColorLineWithText struct {
+	LineWidth int
+	R         int
+	G         int
+	B         int
+	A         int
+	Threshold int
+	HoverText string
+	latlon    string
+}
+
+func (c *ColorLineWithText) String() string {
+	var res string
+	res = fmt.Sprintf(
+		"Color: %d %d %d %d\nLine: %d,,%s\n",
+		c.R, c.G, c.B, c.A,
+		c.LineWidth, c.HoverText,
+	)
+	res += c.latlon
+	res += "End:\n"
+	return res
+}
+
+func (c *ColorLineWithText) AddRawLatLon(ll string) {
+	c.latlon = ll
+}
+
+func (c *ColorLineWithText) MarshalYAML() (interface{}, error) {
+	return c.String(), nil
+}
